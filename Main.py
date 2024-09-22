4ec6eff6efdfce8d917a0657b633487578c7f7e9
Jivin Nayak <jivin.nayak@gmail.com>
2024-09-22 20:04:35 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,16 @@
+class Goat:
+    def Sound(self):
+        print("Mehh")
+
+
+class Tiger:
+    def Sound(self):
+        print("Roar")
+
+
+Jhonny = Tiger()
+
+Ronaldo = Goat()
+
+Jhonny.Sound()
+Ronaldo.Sound()
